7a94a64d9fba08cde27f58e7411de8583c5cb81c
vishnu1229 <knp19cs044@ceknpy.ac.in>
2023-05-01 02:04:07 +0530

    new up

--- context/address.js
+++ context/address.js
@@ -1,3 +1,3 @@
 import paymentgateway from "./PaymentGateway.json"
-export const MarketAddress = '0x1744efd404D93f352Be0028D49DA7f9e54979976';
+export const MarketAddress = '0x509010dC3309627b0a82ce6a8aDaA4cdB3194926';
 export const MarketAddressABI = paymentgateway.abi;
